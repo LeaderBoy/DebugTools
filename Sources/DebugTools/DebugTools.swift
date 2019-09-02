489521f8237f4c26b0e52b4498ab3547f5a9bed1
LeaderBoy <748603349@qq.com>
2019-09-02 16:11:05 +0800

    1.增加ModelDescription 结构化打印数据

--- Sources/DebugTools/DebugTools.swift
+++ Sources/DebugTools/DebugTools.swift
@@ -42,3 +42,46 @@ public func DLog<T>(_ message : T,type : LogType = .default,fileName : String = 
     print("\(type.rawValue)文件:\(logString) \n方法:\(methodName) \n行数:\(lineNumber) \n数据:\(message) \n")
     #endif
 }
+
+
+/*
+ 文件作用: 结构化打印model数据
+ 使用方式: 直接遵循协议即可,比如
+ struct Item : Decodable,ModleDescription{
+ var archive_url : String
+ var archived : Bool
+ var assignees_url : String
+ }
+ 
+ 打印如下
+ Item :
+ - archive_url = https://api.github.com/repos/zestyping/q/{archive_format}{/ref}
+ - archived = false
+ - assignees_url = https://api.github.com/repos/zestyping/q/assignees{/user}
+ */
+protocol ModleDescription : CustomStringConvertible,CustomDebugStringConvertible {}
+
+extension ModleDescription {
+    var debugDescription: String {
+        // 获取 类/结构体 名称
+        let name = String(describing: type(of: self))
+        let mirror = Mirror(reflecting: self)
+        // 描述
+        var des = "\(name) : \n"
+        for children in mirror.children {
+            guard let label = children.label else {
+                continue
+            }
+            let value = children.value
+            des += " - \(label) = \(value) \n"
+        }
+        des += "\n"
+        
+        return des
+    }
+    
+    var description: String {
+        return debugDescription
+    }
+}
+
